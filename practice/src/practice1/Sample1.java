3fc9077686488d072c0a8dbdc2fa5b52196198f3
asd <asd@Adarsh>
2021-03-21 16:25:13 +0530

    uff

---
+++ practice/src/practice1/Sample1.java
@@ -0,0 +1,20 @@
+package practice1;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		int n=3;
+		for(int i=0;i<=n;i++) {
+			for(int j=0;j<=n;j++) {
+				if(i+j<=n-2) {
+				    System.out.println(" ");
+				}
+				for(j=0;j<=n;j++) {
+					
+				}
+			}
+		}
+
+	}
+
+}
